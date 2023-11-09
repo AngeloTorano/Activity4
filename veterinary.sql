25b2f7ad1e2a23482f67bb3d3522889379b87e4a
AngeloTorano <jelotorano17@gmail.com>
2023-11-09 21:28:15 +0800

    feat/insert-10-rows-appointments

--- veterinary.sql
+++ veterinary.sql
@@ -164,3 +164,16 @@ VALUES
 UPDATE doctors
 SET phone = '5552-525-5675', email = 'notnewemail@gmail.com'
 WHERE doctorid = 1;
+
+INSERT INTO appointments(appointid, animalid, appointdate, reason)
+VALUES
+(1, 1, '2023-01-05', 'Annual checkup'),
+(2, 2, '2023-01-10', 'Vaccination'),
+(3, 3, '2023-02-02', 'Injury assessment'),
+(4, 4, '2023-02-15', 'Dental cleaning'),
+(5, 5, '2023-03-05', 'Skin condition'),
+(6, 6, '2023-03-10', 'Check for fleas'),
+(7, 7, '2023-04-12', 'Vaccination'),
+(8, 8, '2023-04-18', 'Spaying/neutering'),
+(9, 9, '2023-05-02', 'Allergy treatment'),
+(10, 10, '2023-05-20', 'Eye infection');
